37e8ac714d9b5167fac33d7fc7deb3f0e9daef30
Utkarsh Katiyar <utkarsh.katiyar@almasaservices.com>
2023-08-13 02:32:15 +0530

    1st try reload26

---
+++ ios/NotificationServices/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationServices
+//
+//  Created by Utkarsh.Katiyar on 26/09/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
