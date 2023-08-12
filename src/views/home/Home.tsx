c4c74d9ab1903248899e7b5cdec9ab795f61275a
Utkarsh Katiyar <utkarsh.katiyar@almasaservices.com>
2023-08-12 12:54:45 +0530

    ios code

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,11 @@
+import Box from "../../restyle/Box"
+import Text from "../../restyle/Text"
+
+const HomeScreen = () => {
+    return (
+        <Box>
+            <Text>hello you are on home page </Text>
+        </Box>
+    )
+}
+export default HomeScreen
